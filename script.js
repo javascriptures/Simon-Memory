bd098f265084f2d4521befc02fbcbd6defbb707b
jordant <jtjavascripture@gmail.com>
2020-01-06 13:41:43 -0800

    buttons made

---
+++ script.js
@@ -0,0 +1,9 @@
+var button1 = document.querySelector('#button1');
+var button2 = document.querySelector('#button2');
+var button3 = document.querySelector('#button3');
+var button4 = document.querySelector('button4');
+button1.addEventListener
+
+('click', function() {
+  console.log('clicked!');
+});
