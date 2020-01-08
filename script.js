24b8909b73a9deeb7abeb7d154b6f58b72e0a8bb
jordant <jtjavascripture@gmail.com>
2020-01-07 17:43:02 -0800

    buttons finished?

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 userInput = [];
 compSeq = [];
 var id = 0;
+const clickSound = new Audio('/sounds/click.mp3');
 
 var readyButton = document.querySelector('.ready');
 // var button1 = document.querySelector('#button1');
 // var button2 = document.querySelector('#button2');
 // var button3 = document.querySelector('#button3');
 // var button4 = document.querySelector('button4');
-var clickSound = document.getElementById('.click');
 
 readyButton.addEventListener('click', readyState());
 
-button1.addEventListener('click', function() {
+document.querySelector('#button1').addEventListener('click', function(event) {
   console.log('clicked!');
-  click();
-});
-readyButton.addEventListener('click', readyState);
-
-function click() {
   clickSound.play();
-}
+  userInput.push(1);
+  clickSound.currentTime = 0;
+  console.log(userInput);
+});
+
+document.querySelector('#button2').addEventListener('click', function(event) {
+  console.log('clicked!');
+  clickSound.play();
+  userInput.push(2);
+  clickSound.currentTime = 0;
+  console.log(userInput);
+});
+
+document.querySelector('#button3').addEventListener('click', function(event) {
+  console.log('clicked!');
+  clickSound.play();
+  userInput.push(3);
+  clickSound.currentTime = 0;
+  console.log(userInput);
+});
+
+document.querySelector('#button4').addEventListener('click', function(event) {
+  console.log('clicked!');
+  clickSound.play();
+  userInput.push(4);
+  clickSound.currentTime = 0;
+  console.log(userInput);
+});
+
+readyButton.addEventListener('click', readyState);
 
 //start sequence
 function readyState() {
   getRandomNumber();
   var i = 0;
   var myInterval = setInterval(function() {
-    id = simonSeq[i];
+    id = compSeq[i];
     button = document.querySelector(`#button${i}`);
   }, 1000);
 }
@@ -34,7 +58,7 @@ function readyState() {
 //generate random number for computer sequence
 function getRandomNumber() {
   var random = Math.floor(Math.random() * 4);
-  simonSeq.push(random);
+  compSeq.push(random);
 }
 
 function sequence() {}
