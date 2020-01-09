04dffde0fece19c225bcc183fac071f3943ff323
jordant <jtjavascripture@gmail.com>
2020-01-08 16:11:02 -0800

    sequence logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 userInput = [];
 compSeq = [];
 var id = 0;
+let buttonCount = compSeq.length;
 const clickSound = new Audio('/sounds/click.mp3');
 
 var readyButton = document.querySelector('.ready');
@@ -9,7 +10,7 @@ var readyButton = document.querySelector('.ready');
 // var button3 = document.querySelector('#button3');
 // var button4 = document.querySelector('button4');
 
-readyButton.addEventListener('click', readyState());
+// readyButton.addEventListener('click', readyState());
 
 document.querySelector('#button1').addEventListener('click', function(event) {
   console.log('clicked!');
@@ -43,23 +44,71 @@ document.querySelector('#button4').addEventListener('click', function(event) {
   console.log(userInput);
 });
 
-readyButton.addEventListener('click', readyState);
+document.querySelector('.sequence').addEventListener('click', function() {
+  var mynewInterval = setInterval(isEqual, 1000);
+});
+
+// readyButton.addEventListener('click', readyState);
 
 //start sequence
-function readyState() {
+// function readyState() {
+//   getRandomNumber();
+//   var i = 0;
+//   var myInterval = setInterval(function() {
+//     id = compSeq[i];
+//     button = document.querySelector(`#button${i}`);
+//   }, 1000);
+// }
+
+function sequence() {
   getRandomNumber();
-  var i = 0;
-  var myInterval = setInterval(function() {
-    id = compSeq[i];
-    button = document.querySelector(`#button${i}`);
-  }, 1000);
 }
 
 //generate random number for computer sequence
 function getRandomNumber() {
-  var random = Math.floor(Math.random() * 4);
+  var random = Math.floor(Math.random() * 4 + 1);
   compSeq.push(random);
 }
+getRandomNumber();
+getRandomNumber();
+getRandomNumber();
+console.log(compSeq);
 
-function sequence() {}
-// 2 user replicates sequence
+
+// set up a function that will light up the sequence
+// function colorSequence() {
+//   for (i = 0; i < compSeq.length; i++) {
+//       buttonColor = document.getElementById(`button${compSeq[i]}`);
+//       console.log(buttonColor);
+//   }
+// }
+var i = 0;
+var myInterval = setInterval(isEqual, 1000);
+function isEqual() {
+  // if the user's input is correct
+  if (userInput == compSeq) {
+    buttonColor = document.getElementById(`button${compSeq[i]}`);
+    buttonColor.style.opacity = '0.25';
+    i++;
+    console.log(button);
+    // // call function to continue sequence
+    // getRandomNumber();
+  }
+}
+// function lightDown() {
+//   var i = 0;
+//   var myInterval = setInterval(function () {
+//       if (i <= compSeq.length) {
+//         document.getElementById(`#button${compSeq[i]}`).style.opacity = '1.0';
+//         i++;
+//         lightUp()
+//       }
+//       else {
+//         clearInterval(myInterval);
+//       }
+//   }, 1000)
+//   }
+
+// function lightDown(){
+
+// }
